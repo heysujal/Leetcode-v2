2ce604f0494d953fa7839b1e58ad49b1e73bb8bd
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-07-29 13:49:14 +0530

    Time: 3 ms (78.84%), Space: 13.8 MB (47.08%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,49 +1,21 @@
 class Solution {
 public:
-    bool isValid(string &s) {
-        stack<char> st;
-        for(int i=0; i<s.size(); i++){
-            char ch = s[i];         
-            if(ch == '(' || ch == '{' || ch == '[')
-                st.push(ch);
-            else{
-                //closing bracket
-                if(!st.empty()){
-                    char top = st.top();
-                    if((ch == ')' && top == '(') || (ch == '}' && top == '{') || 
-                       (ch == ']' &&  top == '['))
-                        st.pop();                        
-                    else
-                        return false;
-                } 
-                else
-                    return false;
-            }
-        }         
-        return st.empty();
-    }
-
-    void solve(int idx, string &temp, int n, vector<string> &ans){
-        if(idx == 2*n){
-            if(isValid(temp))
-                ans.push_back(temp);
+    void solve(int open, int close, string temp, int n, vector<string> &ans){
+        if(close == n){
+            ans.push_back(temp);
             return;
         }
-        // pick '('
-        temp.push_back('(');
-        solve(idx+1, temp, n, ans);
-        temp.pop_back();
-        
-        // pick ')'
-        temp.push_back(')');
-        solve(idx+1, temp, n, ans);
-        temp.pop_back();    
+        if(open < n)
+            solve(open+1, close, temp+"(", n, ans);
+        if(close < open)
+            solve(open, close+1, temp+")", n , ans);
     }
 
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        string temp;
-        solve(0,temp,n,ans);
+        // Opening brackets -> 0
+        // Closing Brackets -> 0
+        solve(0,0,"",n,ans);
         return ans;
     }
 };
