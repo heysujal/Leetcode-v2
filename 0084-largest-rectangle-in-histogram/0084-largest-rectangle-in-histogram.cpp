46c016b131ee120012a3b75884a3dfe064bdea8a
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-07-30 22:27:07 +0530

    Time: 174 ms (92.28%), Space: 81.6 MB (41.04%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int n;
+    void fillNextSmaller(vector<int> &height, vector<int> &next){
+        stack<int> st;
+        for(int i = n-1; i >= 0; i--){
+            if(st.empty()){
+                st.push(i);
+                next[i] = n;
+            }
+            else if(height[st.top()] < height[i]){
+                next[i] = st.top();
+                st.push(i);
+            }
+            else if(height[st.top()] >= height[i]){
+                while(!st.empty() and height[st.top()] >= height[i])
+                    st.pop();
+                if(st.empty()){
+                    next[i] = n;
+                    st.push(i);
+                }else{
+                    next[i] = st.top();
+                    st.push(i);
+                }
+            }
+        }
+    }
+    void fillPrevSmaller(vector<int> &height, vector<int> &prev){
+        stack<int> st;
+        for(int i = 0; i < n; i++){
+            if(st.empty()){
+                st.push(i);
+                prev[i] = -1;
+            }
+            else if(height[st.top()] < height[i]){
+                prev[i] = st.top();
+                st.push(i);
+            }else if(height[st.top()] >= height[i]){
+                while(!st.empty() and height[st.top()] >= height[i])
+                    st.pop();
+                if(st.empty()){
+                    prev[i] = -1;
+                    st.push(i);
+                }else{
+                    prev[i] = st.top();
+                    st.push(i);
+                }
+            }
+        }
+    }
+    int largestRectangleArea(vector<int>& height) {
+        int area = 0;
+        n =  height.size();
+        vector<int> next(n);
+        vector<int> prev(n);
+        fillNextSmaller(height,next);
+        fillPrevSmaller(height,prev);
+        for(int i=0;i<n;i++){
+            int width = 1;
+            area = max(area,width*height[i]);
+            width = (next[i]-1) - (prev[i]+1) + 1;
+            area = max(area, width*height[i]);
+        }
+        return area;
+    }
+};
