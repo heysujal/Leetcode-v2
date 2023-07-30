9e1db3fbfe0b1c13e32bbf92e7148f02aef4ee14
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-07-30 22:34:22 +0530

    Time: 123 ms (99.62%), Space: 81.5 MB (47.18%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -4,47 +4,28 @@ public:
     void fillNextSmaller(vector<int> &height, vector<int> &next){
         stack<int> st;
         for(int i = n-1; i >= 0; i--){
+            while(!st.empty() and height[st.top()] >= height[i])
+                    st.pop();
             if(st.empty()){
-                st.push(i);
                 next[i] = n;
-            }
-            else if(height[st.top()] < height[i]){
+                st.push(i);
+            }else{
                 next[i] = st.top();
                 st.push(i);
-            }
-            else if(height[st.top()] >= height[i]){
-                while(!st.empty() and height[st.top()] >= height[i])
-                    st.pop();
-                if(st.empty()){
-                    next[i] = n;
-                    st.push(i);
-                }else{
-                    next[i] = st.top();
-                    st.push(i);
-                }
             }
         }
     }
     void fillPrevSmaller(vector<int> &height, vector<int> &prev){
         stack<int> st;
         for(int i = 0; i < n; i++){
+            while(!st.empty() and height[st.top()] >= height[i])
+                st.pop();
             if(st.empty()){
-                st.push(i);
                 prev[i] = -1;
-            }
-            else if(height[st.top()] < height[i]){
+                st.push(i);
+            }else{
                 prev[i] = st.top();
                 st.push(i);
-            }else if(height[st.top()] >= height[i]){
-                while(!st.empty() and height[st.top()] >= height[i])
-                    st.pop();
-                if(st.empty()){
-                    prev[i] = -1;
-                    st.push(i);
-                }else{
-                    prev[i] = st.top();
-                    st.push(i);
-                }
             }
         }
     }
