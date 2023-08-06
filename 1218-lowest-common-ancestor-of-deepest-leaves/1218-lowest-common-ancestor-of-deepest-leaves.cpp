1ea008a443c20277c888d0ca1c527762e4c2e1cc
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-08-06 19:14:02 +0530

    Time: 11 ms (83.17%), Space: 21.1 MB (47.65%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,60 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        if(!root)
-            return {};
-        queue<TreeNode*> q;
-        vector<vector<int>> ans;
-        q.push(root);
-        while(!q.empty()){  
-            int size = q.size();
-            vector<int> v;
-            for(int i = 0; i < size ; i++){
-                auto front = q.front();
-                q.pop();
-                v.push_back(front->val);
-                if(front->left)
-                    q.push(front->left);
-                if(front->right)
-                    q.push(front->right);
-            }
-            ans.push_back(v);
-        }
-        return ans;
+// Make using of height to see the deepest left or right subtree
+// whoever is deepest will be having a lower level in level order traversal
+    int getDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+        return max(getDepth(root->right), getDepth(root->left)) + 1;
     }
-    TreeNode* lowestCommonAncestor(TreeNode* root, int p, int q) {
-        if(!root)
-            return NULL;
-        if(root->val == p or root->val == q)
-            return root;
-        
-        TreeNode* leftLCA = lowestCommonAncestor(root->left,p,q);
-        TreeNode* rightLCA = lowestCommonAncestor(root->right,p,q);
-
-        if(leftLCA and rightLCA)
-            return root;
-        if(leftLCA)
-            return leftLCA;
-        if(rightLCA)
-            return rightLCA;
-        return NULL;      
-    }
-
+    
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        vector<vector<int>> ans;
-        ans = levelOrder(root);
-        vector<int> candi = ans.back();
-        return lowestCommonAncestor(root,candi[0],candi.back());
+        if (!root)
+            return NULL;
+        
+        int right_depth = getDepth(root->right);
+        int left_depth = getDepth(root->left);
+        
+        if (right_depth == left_depth)
+            return root;
+        if (right_depth > left_depth) 
+            return lcaDeepestLeaves(root->right);
+        else 
+            return lcaDeepestLeaves(root->left);
     }
 };
