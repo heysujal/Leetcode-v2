fcdd39c8265c7e3ae38575b43fbbc4dcf80eae13
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-08-07 16:45:45 +0530

    Time: 0 ms (100%), Space: 12.1 MB (16.46%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,18 +11,28 @@
  */
 class Solution {
 public:
-
-    void solve(TreeNode* root, int level, vector<int> &ans){
-        if(!root)
-            return;
-        if(level == ans.size())
-            ans.push_back(root->val);
-        solve(root->right,level+1,ans);
-        solve(root->left,level+1,ans);
-    }
+    // Using LevelOrder
     vector<int> rightSideView(TreeNode* root) {
+        if(!root)
+            return {};
+        queue<pair<TreeNode*,int>> q; // node,level
         vector<int> ans;
-        solve(root,0,ans);
+        q.push({root,0});
+        while(!q.empty()){  
+            int size = q.size();
+            for(int i = 0; i < size ; i++){
+                auto front = q.front();
+                TreeNode* node = front.first;
+                int level = front.second;
+                q.pop();
+                if(ans.size() == level)
+                    ans.push_back(node->val);
+                if(node->right)
+                    q.push({node->right,level+1});
+                if(node->left)
+                    q.push({node->left,level+1});
+            }
+        }
         return ans;
     }
 };
