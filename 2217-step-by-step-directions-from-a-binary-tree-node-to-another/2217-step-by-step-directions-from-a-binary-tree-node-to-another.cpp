7b61b509f3b839401ea3edc454178a3247dafee4
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-08-14 00:22:05 +0530

    Time: 158 ms (88.34%), Space: 115.8 MB (33.65%) - LeetHub

---
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* LCA(TreeNode *root, int p, int q){
+        if(!root)
+            return nullptr;
+        // if equal to either then return root
+        if(root->val == p or root->val == q)
+            return root;
+        // else find left and right lca 
+        TreeNode* left = LCA(root->left, p, q);
+        TreeNode* right = LCA(root->right, p, q);
+        if(left and right)
+            return root;
+        if(left)
+            return left;
+        return right;
+    }
+    // lca to node
+    bool lcaToNode(TreeNode* root, int target, string &path){
+        if(!root)
+            return false;
+        if(root->val == target)
+            return true;
+        path += 'L';
+        if(lcaToNode(root->left, target, path))
+            return true;
+        path.pop_back();
+        path += 'R';
+        if(lcaToNode(root->right, target, path))
+            return true; 
+        path.pop_back();
+        return false;
+    }
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+        // get lca of startValue and destination
+        TreeNode* lca = LCA(root, startValue, destValue);
+        string lcaToStart = "";
+        string lcaToDest = "";
+        lcaToNode(lca, startValue, lcaToStart);
+        lcaToNode(lca, destValue, lcaToDest);
+        for(auto &x : lcaToStart)
+            x = 'U';
+        return lcaToStart + lcaToDest;
+    }
+};
