7fd3e6f83e439a2938a6802e4cc51f28aed28cef
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-08-19 19:30:59 +0530

    Time: 4 ms (69.97%), Space: 8.9 MB (57.29%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    vector<int> ans;
-    void nextGE(vector<int> &nums2){
-        stack<int> st;
-        int n = nums2.size();
-        ans.resize(n);
-        for(int i = n - 1 ; i >= 0 ;i--){      
-            while(!st.empty() and nums2[i] >= st.top()){
-                st.pop();
-            }
-            if(st.empty()){
-                ans[i] = -1;
-                st.push(nums2[i]);
-            }
-            else{
-                ans[i] = st.top();
-                st.push(nums2[i]);
-            }
-        }
-    }
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> v;
-        unordered_map<int,int> mp; // value, index for nums2
-        int k = 0;
-        for(auto &x : nums2)
-            mp[x] = k++;
-        nextGE(nums2);
-        for(int i = 0; i < nums1.size(); i++){
-            int idx = mp[nums1[i]];
-            v.push_back(ans[idx]);
+        unordered_map<int,int> mp; // num2[i] -> ans[i]
+        stack<int> st;
+        for(int i = nums2.size()-1; i >= 0; i--){
+            if(st.empty()){
+                mp[nums2[i]] = -1;
+                st.push(nums2[i]);
+            }else{
+                // non-empty
+                while(!st.empty() and st.top() <= nums2[i])
+                    st.pop();
+                if(st.empty()){
+                    mp[nums2[i]] = -1;
+                    st.push(nums2[i]);
+                }
+                else{
+                    mp[nums2[i]] = st.top();
+                    st.push(nums2[i]);
+                }
+            }
         }
-        return v;
+        int n = nums1.size();
+        vector<int> ans(n);
+        for(int i = 0; i < n; i++){
+            ans[i] = mp[nums1[i]];
+        }
+        return ans;
     }
 };
