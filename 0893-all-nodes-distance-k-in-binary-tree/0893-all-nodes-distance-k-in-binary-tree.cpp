7de24dd94322992c793cac83f93508c697f8539d
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-08-22 18:23:55 +0530

    Time: 7 ms (63.07%), Space: 15.3 MB (11.46%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,36 +9,40 @@
  */
 class Solution {
 public:
-    vector<int> ans;   
-    unordered_map<TreeNode*, TreeNode*> parent;  // son=>parent  
-    unordered_set<TreeNode*> visit;    //record visied node  
-    void findParent(TreeNode* node){
-        if(!node ) return;
-        if( node->left ){
-            parent[node->left] = node;
-            findParent(node->left);
-        }
-        if( node->right){
-            parent[node->right] = node;
-            findParent(node->right);
-        }
+    vector<int> ans;
+    unordered_map<TreeNode*,TreeNode*> mp;
+    unordered_map<TreeNode*,bool> vis;
+    bool flag = true;
+    void fillMap(TreeNode* root, TreeNode* parent){
+        if(!root) return;
+        mp[root] = parent;
+        fillMap(root->left, root);
+        fillMap(root->right, root);
     }
-    void dfs(TreeNode* node, int K) {
-        if (!node) return;
-        if (visit.count(node) > 0) return;
-        visit.insert(node);
-        if (K == 0) {
-            ans.push_back(node->val);
-            return;
+    void bfs(TreeNode* root, TreeNode* target, int k){
+        if(!root) return;
+        queue<pair<TreeNode*,int>> q;
+        q.push({target,k});
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                auto node = q.front().first;
+                int K = q.front().second;
+                q.pop();
+                if(!vis[node]){
+                    vis[node] = true;
+                    if(K==0){ ans.push_back(node->val);}
+                    if(node->left) q.push({node->left,K-1});
+                    if(node->right) q.push({node->right,K-1});
+                    if(mp[node]) q.push({mp[node],K-1});
+                }
+                
+            }
         }
-        dfs(node->left, K - 1);
-        dfs(node->right, K - 1);
-        dfs(parent[node], K - 1);
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        if( !root ) return {};
-        findParent(root);
-        dfs(target,k);
+        fillMap(root, nullptr);
+        bfs(target, target, k);
         return ans;
     }
 };
