94e60435c80db50c923b689bfa586dc25dac8b37
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-08-27 22:35:17 +0530

    Time: 11 ms (26.08%), Space: 15.3 MB (11.5%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -12,7 +12,6 @@ public:
     vector<int> ans;
     unordered_map<TreeNode*,TreeNode*> mp;
     unordered_map<TreeNode*,bool> vis;
-    bool flag = true;
     void fillMap(TreeNode* root, TreeNode* parent){
         if(!root) return;
         mp[root] = parent;
@@ -24,20 +23,14 @@ public:
         queue<pair<TreeNode*,int>> q;
         q.push({target,k});
         while(!q.empty()){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                auto node = q.front().first;
-                int K = q.front().second;
-                q.pop();
-                if(!vis[node]){
-                    vis[node] = true;
-                    if(K==0){ ans.push_back(node->val);}
-                    if(node->left) q.push({node->left,K-1});
-                    if(node->right) q.push({node->right,K-1});
-                    if(mp[node]) q.push({mp[node],K-1});
-                }
-                
-            }
+            auto node = q.front().first;
+            int K = q.front().second;
+            q.pop();
+            if(K==0){ ans.push_back(node->val);}
+            if(node->left and !vis[node->left]) q.push({node->left,K-1});
+            if(node->right and !vis[node->right]) q.push({node->right,K-1});
+            if(mp[node] and !vis[mp[node]]) q.push({mp[node],K-1});
+            vis[node] = true;
         }
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
