6ab3f85fd8645804ab8bf5304a0ed4e602f28eca
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-09-07 00:45:47 +0530

    Time: 375 ms (95.43%), Space: 134.8 MB (93.3%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
- 
-    bool validPath(int n, vector<vector<int>>& edges, int source, int dest) {
-        // make a adjacency list
-        vector<int> adj[n];
-        vector<bool> vis(n, false);
-        for(int i = 0; i < edges.size(); i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+    vector<int> parent; // mapping for node to parent
+    vector<int> rank;
+    int findParent(int node){
+        if(parent[node] == node)
+            return node;
+        return findParent(parent[node]); 
+    }
+    void makeParent(int u, int v){
+        int pu = findParent(u);
+        int pv = findParent(v);
+        if(rank[pu] < rank[pv]){
+            parent[pu] = pv;
         }
-        queue<int> q;
-        q.push(source);
-        while(!q.empty()){
-            auto front = q.front();
-            q.pop();
-            vis[front] = true;
-            if(front == dest)
-                return true;
-            for(auto it : adj[front])
-                if(!vis[it])
-                    q.push(it);
+        else if(rank[pu] > rank[pv]){
+            parent[pv] = pu;
         }
-        return false;
+        else{
+            parent[pu] = pv; // pick anyone as parent
+            rank[pv]++;
+        }
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        parent.resize(n);
+        rank.resize(n);
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+        for(auto e : edges){
+            makeParent(e[0], e[1]);
+        }
+        return findParent(source) == findParent(destination);
     }
 };
