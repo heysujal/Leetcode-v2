a01a1423da577d3ac90a44e3d0e603d677e819ab
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-09-13 16:19:46 +0530

    Time: 117 ms (39.79%), Space: 61.8 MB (25.55%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int i = 0;
-        int n = nums.size();
-        while(i < n){
-            // 1 should be present at index 0 in sorted order
-            if(nums[i]-1 < n and nums[i] != nums[nums[i]-1])
-                swap(nums[i], nums[nums[i]-1]);
-            else
-                i++;
-        }
-        // by this time order should be like 1 2 3 4 .....N X(this X is repeated number)
-        // so the wrong number should be present at end (Why?)
-        // Range is 1 to N but there are n+1 numbers in total
-        // one number is being repeated one or many number of times
-        // so more number would be lost 
-        // the repeated number will take the place of loss numbers
-        // and also would be present at the end
-        return nums[n-1];
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < nums.size(); i++)
+            if(nums[i-1] == nums[i])
+                return nums[i];
+        return -1;
     }
 };
