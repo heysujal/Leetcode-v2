6eb93e333b61fb4a6f6969d88ce2882c9ad6c800
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-09-13 16:12:13 +0530

    Time: 255 ms (6.78%), Space: 61.5 MB (26.65%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -10,8 +10,16 @@ public:
             else
                 i++;
         }
-        // by this time order should be like 1 2 3 4 .....
-        // iterating over range
+        for(auto x : nums)
+            cout<< x << " ";
+
+        // by this time order should be like 1 2 3 4 .....N X(this X is repeated number)
+        // so the wrong number should be present at end (Why?)
+        // Range is 1 to N but there are n+1 numbers in total
+        // one number is being repeated one or many number of times
+        // so more number would be lost 
+        // the repeated number will take the place of loss numbers
+        // and also would be present at the end
         return nums[n-1];
     }
 };
