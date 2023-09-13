252750ad58d5dcbe986b1de60a10f857d0bf5549
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-09-13 22:53:04 +0530

    Time: 55 ms (57.82%), Space: 25 MB (22.7%) - LeetHub

--- 1117-as-far-from-land-as-possible/1117-as-far-from-land-as-possible.cpp
+++ 1117-as-far-from-land-as-possible/1117-as-far-from-land-as-possible.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
     int n;
+    vector<vector<int>> dir = {{0,1},{0,-1},{1,0},{-1,0}};
     int maxDistance(vector<vector<int>>& grid) {
         n = grid.size();
-        queue<pair<int,int>> q;
+        int water = 0;
+        int land = 0;
+        vector<vector<int>> vis(n,vector<int>(n,0));
+        vector<vector<int>> dist(n,vector<int>(n,0));
+        queue<pair<pair<int,int>,int>> q;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1)
-                    q.push({i,j});
-            }
-        }
-        int res = 0;
-        while(!q.empty()){
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            vector<vector<int>> dir = {{0,1},{0,-1},{1,0},{-1,0}};
-            for(auto &d : dir){
-                int new_x = x + d[0];
-                int new_y = y + d[1];
-                if(new_x >= 0 and new_y >= 0 and new_x < n and new_y < n and grid[new_x][new_y]==0){
-                    q.push({new_x, new_y});
-                    grid[new_x][new_y] = grid[x][y] + 1;
-                    res = max(res, grid[new_x][new_y]);
+                if(grid[i][j] == 1){
+                    land++;
+                    q.push({{i,j},0});
+                    vis[i][j] = true;
+                }
+                else{
+                    water++;
                 }
             }
         }
-        return res - 1;
+        if(land == 0 or water == 0)
+            return -1;
+        int res = 0;
+        while(!q.empty()){
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int distance = q.front().second;
+            dist[x][y] = distance; 
+            q.pop();
+            for(auto &d : dir){
+                int new_x = x + d[0];
+                int new_y = y + d[1];
+                if(new_x >= 0 and new_y >= 0 and new_x < n and new_y < n and vis[new_x][new_y]!=1){
+                    q.push({{new_x, new_y},distance+1});
+                    vis[new_x][new_y] = true;
+                    res = max(res, distance+1);
+                }
+            }
+        }
+        return res;
     }
 };
