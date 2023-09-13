2f2463801125cad7b4707f4641fb475c12612124
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-09-13 15:21:49 +0530

    Time: 40 ms (81%), Space: 34 MB (51.24%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        // apply cyclic sort
+        // idea nums[i] should be present at index nums[i]-1
+        // 1 should be present at index 1-1 = 0
+
+        int i = 0;
+        int n = nums.size();
+        vector<int> ans;
+        while(i < n){
+            if(nums[i]-1 < n and nums[i] != nums[nums[i]-1])
+                swap(nums[i], nums[nums[i]-1]);
+            else
+                i++;
+        }
+        for(int i = 1; i <= n; i++){
+            if(i != nums[i-1])
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
