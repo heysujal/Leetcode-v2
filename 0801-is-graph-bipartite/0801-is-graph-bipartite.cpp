0df06ae598de65cfd61316efc1dd8db25c6fb69b
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-09-15 18:49:34 +0530

    Time: 11 ms (98.86%), Space: 13.7 MB (36.82%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    bool bipartite(int i, vector<vector<int>> &graph, vector<int> &color){
-        if(color[i] == -1)
-            color[i] = 1;
-        for(auto &it : graph[i]){
+    bool bipartite(int node, int col, vector<int> &color, vector<vector<int>>& adj){
+        color[node] = col;
+        for(auto it : adj[node]){
             if(color[it] == -1){
-                color[it] = 1 - color[i];
-                if(!bipartite(it,graph,color))
+                if(!bipartite(it, !col, color, adj))
                     return false;
             }
-            else if(color[it] == color[i]){
+            else if(color[it] == color[node])
                 return false;
-            }
         }
         return true;
     }
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> color(n);
-        for(auto &e : color) e = -1;
+    bool isBipartite(vector<vector<int>>& adj) {
+        int n = adj.size();
+        vector<int> color(n, -1);
         for(int i = 0; i < n; i++){
             if(color[i] == -1)
-                if(!bipartite(i,graph,color))
+                if(!bipartite(i, 0, color, adj))    
                     return false;
         }
         return true;
