06369102bf60c5f60350078bbdb39059002b2d27
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-12-24 22:28:46 +0530

    Time: 3 ms (78.34%), Space: 7.3 MB (86.65%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,23 +2,14 @@ class Solution {
 public:
     int minOperations(string s) {
         int n = s.size();
-        string possible_string1 = "";
-        string possible_string2 = "";
         char flag1 = '0';
         char flag2 = '1';
-        for(int i=0; i < n; i++){
-            possible_string1.push_back(flag1);
-            possible_string2.push_back(flag2);
-            swap(flag1, flag2);
-        }
-
         int count1 = 0;
         int count2 = 0;
-        for(int i = 0; i < n; i++){
-            if(s[i] != possible_string1[i])
-                count1++;
-            if(s[i] != possible_string2[i])
-                count2++;
+        for(int i=0; i < n; i++){
+            if(s[i] != flag1) count1++;
+            if(s[i] != flag2) count2++;
+            swap(flag1, flag2);
         }
         return min(count1, count2);
     }
