9ba1643ab3599befb0c3cd987e2298ebd5865c78
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-12-26 12:24:05 +0530

    Time: 104 ms (5.29%), Space: 8.8 MB (65.29%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     const int mod = 1e9 + 7;
     int solve(int i, int n, int k, int target, vector<vector<int>> &dp){
-        if(i == n){
+        if(i == n){ // we are out of dice here 
             return target == 0 ? 1 : 0;
         }
         if(dp[i][target] != -1)
@@ -16,9 +16,35 @@ public:
         return dp[i][target] = ways;
     }
 
+    int solveTab(int n, int k, int target){
+        vector<vector<int>> dp(n, vector<int>(target+1, 0));
+        // what is dp[i][j] here?
+        // dp[i][j] is ways to form target j using exactly i..n-1 dice
+        // here this means if you can form target using a single last die
+        // so, only 1, 2, 3, 4, 5, and 6 can be formed with one die
+        // filling up the base case
+        for(int face = 1; face <= target; face++){
+            if(face >= 1 and face <= k)
+                dp[n-1][face] = 1;
+        }
+
+        for(int i = n-2; i >= 0; i--){
+            for(int t = 0; t <= target; t++){
+                for(int face = 1; face <= k; face++){
+                    if(t - face >= 0){
+                        int ways = dp[i+1][t-face];
+                        dp[i][t] += ways;
+                        dp[i][t] = dp[i][t] % mod;
+                    }
+                }
+            }
+        }
+        return dp[0][target];
+    }
     int numRollsToTarget(int n, int k, int target) {
         // starting with 0th die
-        vector<vector<int>> dp(n, vector<int>(target+1, -1));
-        return solve(0, n, k, target, dp);
+        // vector<vector<int>> dp(n, vector<int>(target+1, -1));
+        // return solve(0, n, k, target, dp);
+        return solveTab(n, k, target);
     }
 };
