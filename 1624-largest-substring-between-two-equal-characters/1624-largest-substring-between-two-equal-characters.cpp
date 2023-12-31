e2f4af632b6adb8c45d5b738408f8c53a8ed48ea
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-12-31 17:20:07 +0530

    Time: 3 ms (47.38%), Space: 6.8 MB (22.19%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
         int len = INT_MIN;
-        unordered_map<char, vector<int>> mp;
-        int idx = 0;
-        for(auto ch : s){
-            mp[ch].push_back(idx);
-            idx++;
+        int n = s.size();
+        vector<int> firstIdx(26, -1);
+        vector<int> lastIdx(26, -1);
+        for(int i = 0; i < n; i++){
+            if(firstIdx[s[i] - 'a'] != -1)
+                continue;
+            firstIdx[s[i] - 'a'] = i;
         }
-        bool flag = false;
-        for(auto &p : mp){
-            auto v = p.second;
-            int size = v.size();
-            if(size > 1)
-                flag = true;
-            int f = v[0];
-            int l = v[v.size() - 1];
+        for(int i = n-1; i >= 0; i--){
+            if(lastIdx[s[i] - 'a'] != -1)
+                continue;
+            lastIdx[s[i] - 'a'] = i;
+        }
+
+        for(auto ch : s){
+            int f = firstIdx[ch - 'a'];
+            int l = lastIdx[ch - 'a'];
             len = max(len, l - f - 1);
         }
-        if(!flag)
-            return -1;
         return len;
     }
 };
