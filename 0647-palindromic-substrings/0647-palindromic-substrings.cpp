88b0e65bd3922250cccc2cef7ff13a66f7251695
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-04 00:52:10 +0530

    Time: 34 ms (22.62%), Space: 8.1 MB (54.42%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    int expandAroundCentre(int i, int j, string &s){
-        int n = s.size();
-        int count = 0;
-        while((i >= 0 and j < n) and (s[i] == s[j])){
-            count++;
-            i--;
-            j++;
-        }
-        return count;
-    }
+
+    // Tabulation solution is all about iterating diagonally
+    // short ref : https://youtu.be/biHfMygdoCE?t=1089
     int countSubstrings(string s) {
         int n = s.size();
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            int odd = expandAroundCentre(i, i, s);
-            int even = expandAroundCentre(i, i+1, s);
-            ans += (odd + even);
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        // we know all strings of 1 length(i,i) in matrix are
+        // palindromes
+        int count = 0;
+
+        for(int diff = 0; diff < n; diff++){
+            for(int i = 0, j = i + diff; j < n; i++,j++){
+                    if(i == j){ // string of 1 len
+                        dp[i][j] = true;
+                    }
+                    else if(j-i == 1){  // string of 2 len(j-i+1 = 2)
+                        dp[i][j] = (s[i] == s[j]);
+                    }
+                    else if(i+1 < n and j-1 >= 0){
+                        dp[i][j] = (s[i] == s[j] and dp[i+1][j-1]);
+                    }
+                    if(dp[i][j]){
+                        count += 1;
+                    } // true or false;
+            }
         }
-        return ans;
+        return count;
     }
 };
