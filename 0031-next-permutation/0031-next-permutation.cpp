b54309ae33d6a88c08101497d32fe0a9298cb9de
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-04 14:51:06 +0530

    Time: 4 ms (55.82%), Space: 12.3 MB (92.54%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-// codeStory : https://www.youtube.com/watch?v=CboKBIpo640
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        // first find the right candidate/(i-1)th elements or gola
         int gola_index = -1;
-
-        // go from right to left
-        for(int i = n-1; i > 0; i--){
-            if(nums[i-1] < nums[i]){
-                gola_index = i-1;
+        int n = nums.size();
+        for(int i = n-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                gola_index = i;
                 break;
             }
         }
-
-        if(gola_index != -1){
-            // swap_idx will be just greater element on right side
-            int swap_index = gola_index; // assume, pro: handles when no greater found
-            // to find first greater we traverse from right till gola + 1
-            for(int j = n-1; j >= gola_index + 1; j--){
-                if(nums[gola_index] < nums[j]){
-                    swap_index = j;
-                    break;
-                }
-            }
-            swap(nums[gola_index], nums[swap_index]);
+        if(gola_index == -1){
+            reverse(nums.begin(), nums.end());
+            return;
         }
-        // reverse from [i..end]                        
+        // swap w first greater from right. search Range [gola_index+1.....end]
+        for(int i = n-1; i > gola_index; i--){
+            if(nums[i] > nums[gola_index]){
+                swap(nums[i], nums[gola_index]);
+                break;
+            }
+        }
         reverse(nums.begin() + gola_index + 1, nums.end());
     }
 };
