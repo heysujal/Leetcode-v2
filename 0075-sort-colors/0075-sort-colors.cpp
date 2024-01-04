93cff23d2dfb9e813174081f2e19c5016b8b5af2
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-04 16:50:27 +0530

    Time: 4 ms (32.71%), Space: 8.6 MB (38%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,26 +1,7 @@
 class Solution {
 public:
-    // dnf sort
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int low = 0; // points at 0s
-        int mid = 0; // points at 1s
-        int high = n - 1; // points at 2s
-        
-        while(mid <= high){
-            if(nums[mid] == 0){
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
-            }
-            else if(nums[mid] == 1){
-                mid++;
-            }
-            else if(nums[mid] == 2){
-                swap(nums[mid], nums[high]);
-                high--;
-            }
-        }
+        sort(nums.begin(), nums.end());
         return;
     }
 };
