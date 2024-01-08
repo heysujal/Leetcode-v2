a17c120947985ebb58191d5a626795e0418364aa
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-08 12:51:10 +0530

    Time: 19 ms (99.48%), Space: 19.4 MB (57.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,22 +3,15 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
         int n = arr.size();
         sort(arr.begin(), arr.end());
-        if(n==1)
-            return arr;
         vector<vector<int>> ans;
-        int start = arr[0][0];
-        int end = arr[0][1];
+
         for(int i = 0; i < n; i++){
-            if(arr[i][0] <= end){
-                end = max(end, arr[i][1]);
-            }
-            else{
-                ans.push_back({start, end});
-                start = arr[i][0];
-                end = arr[i][1];
-            }
+            // if curr interval doesn't lie in last interval
+            if(ans.size() == 0 or arr[i][0] > ans.back()[1])
+                ans.push_back(arr[i]);
+            else    // overlap found
+                ans.back()[1] = max(ans.back()[1], arr[i][1]);
         }
-        ans.push_back({start, end});
         return ans;
     }
 };
