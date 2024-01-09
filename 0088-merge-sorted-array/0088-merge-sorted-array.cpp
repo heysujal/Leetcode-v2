16bcde6f45903148a145c1ea36b4658e9c4fc393
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-09 18:27:23 +0530

    Time: 0 ms (100%), Space: 9.6 MB (5.31%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,18 +4,12 @@ public:
         int i = m-1;
         int j = n-1;
         int k = m+n-1;
-        while(i >= 0 and j >= 0) {
-            if(nums1[i] < nums2[j]) {
-                nums1[k--] = nums2[j--];
-            } else {
-                nums1[k--] = nums1[i--];
-            }
-        }
-// let's say if there are some elements left in n1 and n2 is 
-// completely traversed , 
-// then elements of n1 are already in there correct positions.
         while(j >= 0) {
-            nums1[k--] = nums2[j--];
+            if(i >= 0 and nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
         }
     }
 };
