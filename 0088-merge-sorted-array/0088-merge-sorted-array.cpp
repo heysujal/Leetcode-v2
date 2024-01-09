6f7d2bd8e7e61a4c1b8fb8dc29d9baa79b2a1bcd
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-09 18:23:41 +0530

    Time: 5 ms (30.99%), Space: 9.5 MB (40.68%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    // brute
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> arr(m+n);
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        while(i < m and j < n){
-            arr[k++] = (nums1[i] <= nums2[j]) ? nums1[i++] : nums2[j++];
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(i >= 0 and j >= 0) {
+            if(nums1[i] < nums2[j]) {
+                nums1[k--] = nums2[j--];
+            } else {
+                nums1[k--] = nums1[i--];
+            }
         }
-        while(i < m){
-            arr[k++] = nums1[i++];
+// let's say if there are some elements left in n1 and n2 is 
+// completely traversed , 
+// then elements of n1 are already in there correct positions.
+        while(j >= 0) {
+            nums1[k--] = nums2[j--];
         }
-        while(j < n){
-            arr[k++] = nums2[j++];
-        }
-        nums1 = arr;
     }
 };
