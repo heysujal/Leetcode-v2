f58ff7570ee3bb38df365aaa92ba79d7df84a529
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-12 23:55:18 +0530

    Time: 1115 ms (22.73%), Space: 70.9 MB (32.7%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,19 +5,22 @@ public:
         if(!n)
             return 0;
         int ans = 1;
-        int len = 1;
-        sort(nums.begin(), nums.end());
-        for(int i  = 1 ; i < nums.size() ; i++){
-            
-            if(nums[i] == nums[i-1] + 1){
-                len++;
-                ans = max(ans, len);             
-            }else if(nums[i]==nums[i-1]){
-                continue;
-            }else{
-                len = 1;
+         
+        unordered_set<int> st(nums.begin(), nums.end());
+         
+        for(auto x : nums){
+            if(!st.count(x-1)){
+                int currele = x;
+                int currlen = 1;
+                while(st.count(currele+1)){
+                    currlen++;
+                    currele+=1;
+                }
+                ans = max(ans, currlen);
+
             }
         }
+
     return ans;
     }
 };
