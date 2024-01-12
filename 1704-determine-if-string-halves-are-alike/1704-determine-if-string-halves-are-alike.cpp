6ddd15ef7d4e1239a3330a413e831a620d72bac9
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-13 00:05:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    
-    int countVowel(string s, int low , int high) {
-        int count = 0;
-        vector<char> vowels = {
-            'a', 'e', 'i', 'o', 'u'
-        };
-        for(int i = low; i <= high ; i++){
-            char ele = tolower(s[i]);
-            auto it = find(vowels.begin(), vowels.end(), ele);
-            if(it != vowels.end()){
-               count++;
-            }   
-        }
-        return count;
-    }
+
     bool halvesAreAlike(string s) {
-        int limit = (s.size()/2) -1;
-        int c1 = countVowel(s, 0, limit);
-        int c2 = countVowel(s,limit+1, s.size()-1);
-        return c1 == c2;
+        int n = s.size() / 2;
+        string s1 = s.substr(0, n);
+        string s2 = s.substr(n, n);
+        unordered_map<char, int> mp;
+        vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+        for(auto &ch : s1)
+            mp[ch]++;
+        for(auto &ch : s2)
+            mp[ch]--;
+        for(auto &ch : vowels){
+            if(mp[ch] != 0)
+                return false;
+        }
+        return true;
     }
 };
