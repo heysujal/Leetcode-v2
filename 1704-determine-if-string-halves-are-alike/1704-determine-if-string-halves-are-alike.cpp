275111b38224c6572aa2958e72654f506cb9818d
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-12 23:59:10 +0530

    Time: 2 ms (64.23%), Space: 7.3 MB (5.15%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -2,42 +2,23 @@ class Solution {
 public:
     
     int countVowel(string s, int low , int high) {
-        
-        
         int count = 0;
-        
-        
+        vector<char> vowels = {
+            'a', 'e', 'i', 'o', 'u'
+        };
         for(int i = low; i <= high ; i++){
             char ele = tolower(s[i]);
-            
-            if((ele=='a') or
-             ( ele=='e') or
-              (ele=='i') or
-              (ele=='o') or
-              (ele=='u') ){
+            auto it = find(vowels.begin(), vowels.end(), ele);
+            if(it != vowels.end()){
                count++;
-                
-            }
-            
-            
+            }   
         }
-        
-        
         return count;
     }
     bool halvesAreAlike(string s) {
         int limit = (s.size()/2) -1;
-        
         int c1 = countVowel(s, 0, limit);
         int c2 = countVowel(s,limit+1, s.size()-1);
-        
-        if(c1==c2)
-            return true;
-        
-        return false
-            
-            ;
-        
-        
+        return c1 == c2;
     }
 };
