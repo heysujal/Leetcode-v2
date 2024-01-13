0b3c5ecae537e8447f5c07b5e0c5a9b8bd7953b4
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-13 22:19:24 +0530

    Time: 7 ms (89.71%), Space: 16.4 MB (18.01%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        vector<int> ans;
+        // there is no guarantee if element must exist
         int n = nums.size();
-        // Traverse through array elements and
-        // count frequencies
-        for (int i = 0; i < n; i++){
-             mp[nums[i]]++;
+        int count1 = 0;
+        int candidate1 = INT_MIN;
+        int count2 = 0;
+        int candidate2 = INT_MIN;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == candidate1){
+                count1++;
+            }
+            else if(nums[i] == candidate2){
+                count2++;
+            }
+            else if(count1 == 0){
+                candidate1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                candidate2 = nums[i];
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
-        for(auto p : mp){
-            if(p.second > n/3)
-                ans.push_back(p.first);
+        // Verification 
+        int maj = n/3;
+        count1 = 0;
+        count2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == candidate1)
+                count1++;
+            if(nums[i] == candidate2)
+                count2++;
         }
+        vector<int> ans;
+        if(count1 > maj)
+            ans.push_back(candidate1);
+        if(count2 > maj)
+            ans.push_back(candidate2);
         return ans;
     }
 };
