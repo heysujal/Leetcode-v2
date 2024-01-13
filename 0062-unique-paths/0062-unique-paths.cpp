04da8a982e9103fbecef22b16a2abbea50b93855
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-13 22:50:34 +0530

    Time: 3 ms (33.26%), Space: 6.5 MB (78.21%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
-    int solve(int i, int j, int m, int n, vector<vector<int>> &dp) {
-        if (i == m - 1 and j == n - 1)
-            return 1;
-        if (i >= m or j >= n)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        return dp[i][j] = solve(i + 1, j, m, n, dp) + solve(i, j + 1, m, n, dp);
-    }
+
     int solveTab(int m, int n){
         vector<vector<int>> dp(m, vector<int>(n, 0));
-        // dp[i][j] means number of paths from i,j to m-1, n-1
-        // if row is m-1 then dp[m-1] = 1 for all j
-        // which means only way to go is in Right direction
         for(int j = 0; j < n; j++)
             dp[m-1][j] = 1;
         for(int i = m-2; i >= 0; i--){
@@ -25,9 +14,22 @@ public:
         }
         return dp[0][0];
     }
+    int solveSpace(int m, int n){
+        vector<int> prev(n, 0);
+        for(int j = 0; j < n; j++)
+            prev[j] = 1;
+        for(int i = m-2; i >= 0; i--){
+            vector<int> curr(n, 0);
+            for(int j = n-1; j >= 0; j--){
+                curr[j] += prev[j];
+                if(j != n-1)
+                    curr[j] += curr[j+1];
+            }
+            prev = curr;
+        }
+        return prev[0];
+    }
     int uniquePaths(int m, int n) { 
-        // vector<vector<int>> dp(m, vector<int>(n, -1));
-        // return solve(0, 0, m, n, dp); 
-        return solveTab(m, n);
+        return solveSpace(m, n);
     }
 };
