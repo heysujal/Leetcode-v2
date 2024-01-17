8ff79ea2ca1e7e3c4b964f153dee21c326b04e5d
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-17 12:59:23 +0530

    Time: 3 ms (45.12%), Space: 6.4 MB (86.93%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // cacheo solution
+    // Memo solution
     int dp[101];
     int solve(int i, string &s, int n){
         if(i == n){
@@ -20,31 +20,11 @@ public:
         return dp[i] = result;
     }
 
-    int solveTab(string &s){
-        int n = s.size();
-        vector<int> cache(n + 1, 0);
-        cache[n] = 1; // base case
-        for(int i = n-1; i >= 0; i--){
-            if(s[i] == '0'){
-                cache[i] = 0;
-                continue;
-            }
-            cache[i] = cache[i+1];
-            if(i+1 < n){
-                if(s[i] == '1' or (s[i] == '2' and s[i+1] <= '6')){
-                    cache[i] += cache[i+2];
-                }
-            }
-        }
-        return cache[0];
-    }
     int numDecodings(string s) {
-        // int n = s.size();
-        // cacheset(dp, -1, sizeof(dp));
+        int n = s.size();
+        memset(dp, -1, sizeof(dp));
         // if(n == 0)  don't add this check as not in constraints
         //     return 0;
-        // return solve(0, s, n);
-
-        return solveTab(s);
+        return solve(0, s, n);
     }
 };
