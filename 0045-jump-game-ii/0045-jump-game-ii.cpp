7ff69233e04a9e7c076f5602b4765a102092b720
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-23 00:36:13 +0530

    Time: 68 ms (35.55%), Space: 19.7 MB (5.61%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -14,9 +14,29 @@ public:
         }
         return dp[i] = ans;
     }
+    int helperTab(vector<int> &nums){
+        if(n==1)
+            return 0;
+        vector<int> dp(n, 0);
+        // dp[i] represents min steps to go from i to n-1
+        dp[n-1] = 0;
+        dp[n-2] = 1;
+
+        for(int i = n-2; i >= 0; i--){
+            int ans = 1e9;
+            for(int j = 1; j <= nums[i]; j++){
+                if(i + j < n){
+                    ans = min(ans, 1 + dp[i+j]);
+                }
+            }
+            dp[i] = ans;
+        }
+        return dp[0];
+    }
     int jump(vector<int>& nums) {
         n = nums.size();
-        vector<int> dp(n, -1);
-        return helper(0, nums, dp);
+        // vector<int> dp(n, -1);
+        // return helper(0, nums, dp);
+        return helperTab(nums);
     }
 };
