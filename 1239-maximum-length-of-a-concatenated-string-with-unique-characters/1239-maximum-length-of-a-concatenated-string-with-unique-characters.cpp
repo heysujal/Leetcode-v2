abc06894b58bfbe2f0fbed24334833eb768c6566
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-23 14:21:05 +0530

    Time: 47 ms (59.57%), Space: 61.8 MB (36.71%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
-    bool isUnique(string s){
+    bool hasCommon(string &p, string &q){
         vector<int> v(26, 0);
-        for(auto &ch : s){
-            v[ch-'a']++;
-            if(v[ch-'a'] > 1){
-                return false;
+        for(auto &c : p){
+            v[c-'a']++;
+            // also to handle ["aa", "bb"] test case
+            if(v[c-'a'] > 1)
+                return true;
+        }
+        for(auto &c : q){
+            v[c-'a']++;
+            if(v[c-'a'] > 1){
+                return true;
             }
         }
-        return true;
+        return false;
+    }
+    int helper(int i, string temp, vector<string> &arr){
+        if(i >= arr.size()){
+            return temp.size();
+        }
+        // if temp and arr[i] don't have anything in common
+        // then we can include or exclude
+        int include = 0;
+        int exclude = 0;
+        if(!hasCommon(arr[i], temp)){
+            // exclude
+            exclude = helper(i+1, temp, arr);
+            // include
+            include = helper(i+1, temp+arr[i], arr);
+        }
+        else{
+            exclude = helper(i+1, temp, arr);
+        }
+        return max(include, exclude);
     }
     int maxLength(vector<string>& arr) {
-        int n = arr.size();
-        int ans = 0; 
-        if(isUnique(arr[0])){
-            ans = arr[0].size();
-        }
-        for(int i = 0; i < n; i++){
-            string s=arr[i];
-            for(int j = i+1; j < n; j++){
-                string t = s + arr[j];
-                if(isUnique(t)){
-                    // if unique then update s
-                    int k = t.size();
-                    ans = max(ans, k);
-                    s = t;
-                }
-                else{
-                    // do nothing
-                }
-            }
-        }
-        return ans;
+        string temp = "";
+        return helper(0, temp, arr);
     }
 };
