f1d256202c3869053178a6d54c05cd23a2cae1f2
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-25 22:27:01 +0530

    Time: 9 ms (5.2%), Space: 14.5 MB (6.95%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int gola_index = -1;
         int n = nums.size();
-        for(int i = n-2; i >= 0; i--){
-            if(nums[i] < nums[i+1]){
-                gola_index = i;
+        int gola_index = -1;
+        for(int i = n-1; i >= 1; i--){
+            if(nums[i-1] < nums[i]){
+                gola_index = i-1;
                 break;
             }
         }
-        if(gola_index == -1){
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-        // swap w first greater from right. search Range [gola_index+1.....end]
-        for(int i = n-1; i > gola_index; i--){
-            if(nums[i] > nums[gola_index]){
-                swap(nums[i], nums[gola_index]);
-                break;
+        if(gola_index != -1){
+            int swap_index = -1;
+            for(int i = n-1; i > gola_index; i--){
+                if(nums[gola_index] < nums[i]){
+                    swap_index = i;
+                    break;
+                }
             }
+            swap(nums[gola_index], nums[swap_index]);
+            // reverse nums from gola_index + 1 till end
         }
-        reverse(nums.begin() + gola_index + 1, nums.end());
+        reverse(nums.begin() + gola_index+1, nums.end());
     }
 };
