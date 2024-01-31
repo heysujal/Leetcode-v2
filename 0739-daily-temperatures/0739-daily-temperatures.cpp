8a665f23e225c7f9324645ae37ddcc47c5e28bf2
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-01-31 14:29:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n,0); 
-        stack<int> st; // for storing the indices to keep track of greater elements
-        for(int i = n-1; i >= 0; i--){
-            if(!st.empty() and nums[i] < nums[st.top()]){
-                ans[i] = st.top()-i;
+        vector<int> ans(n, 0);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            if(!st.empty() and nums[st.top() > nums[i]]){
+                ans[i] = st.top() - i;
             }
-            while(!st.empty() and (nums[i] >= nums[st.top()]))
+            // if larger
+            while(!st.empty() and nums[st.top()] < nums[i]){
                 st.pop();
-            // if stack becomes empty then what?
-            if(st.empty()) // kuch mat kro
+            }
+            if(st.empty()){
+                st.push(i);
                 ans[i] = 0;
-            else // we have a warmer day at stack ka top
-                ans[i] = st.top()-i; // also save current bcoz it can be first warmer for rest
-            st.push(i);
+            }
+            else{
+                ans[i] = st.top() - i;
+                st.push(i); // finally stack ke andar kuch bada hai phle se hi
+            }
         }
         return ans;
     }
