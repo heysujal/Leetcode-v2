bf53b68d40be6e9c520b6c21edc7a312b2362034
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-01 13:21:15 +0530

    Time: 126 ms (99.41%), Space: 119.5 MB (39.48%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    bool isKDiff(vector<int> &nums, int i, int j, int k){
-        return abs(nums[i] - nums[j]) <= k;
-    }
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
         vector<vector<int>> ans;
         int n = nums.size();
         sort(nums.begin(), nums.end());
         for(int i = 0; i < n; i += 3){
-            if(!isKDiff(nums, i, i+2, k)){
+            if((nums[i+2] - nums[i]) > k){
                 return {};
             }
             else{
-                vector<int> v(nums.begin() + i, nums.begin() + i+3);
-                ans.push_back(v);
+                ans.push_back({nums[i], nums[i+1], nums[i+2]});
             }
         }
         return ans;
