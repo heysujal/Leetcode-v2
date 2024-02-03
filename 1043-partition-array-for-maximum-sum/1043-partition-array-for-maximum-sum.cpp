6056ee2a38020dfc70013c98eb2f6e4916c7fe9c
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-03 13:34:02 +0530

    Time: 7 ms (92.36%), Space: 10.9 MB (23.07%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -21,8 +21,7 @@ public:
 
     int helperTab(vector<int> &arr, int k){
         vector<int> dp(n+1, 0);
-        dp[n-1] = arr[n-1];
-        for(int i = n-2; i >= 0; i--){
+        for(int i = n-1; i >= 0; i--){
             int len = 0;
             int maxi = INT_MIN;
             int sum = INT_MIN;
