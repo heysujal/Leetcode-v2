8d916036824c018e0a4e119d68f0fa80b28a427b
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-06 17:38:43 +0530

    Time: 30 ms (95.78%), Space: 16.9 MB (43.05%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,16 +8,36 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *p, ListNode *q) {
-        unordered_map<ListNode*, bool> mp;
-        while(p){
-            mp[p] = true;
-            p = p->next;
+
+    int length(ListNode *head){
+        int len = 0;
+        ListNode* ptr = head;
+        while(ptr){
+            len++;
+            ptr = ptr->next;
         }
-        while(q){
-            if(mp[q]){
-                return q;
+        return len;
+    }
+    ListNode *getIntersectionNode(ListNode *p, ListNode *q) {
+        int m = length(p);
+        int n = length(q);
+        int diff = abs(m-n);
+        // cover the diff for whoever is longer
+        if(m > n){
+            while(diff--){
+                p = p->next;
             }
+        }
+        else{
+            while(diff--){
+                q = q->next;
+            }
+        }
+        while(p and q){
+            if(p == q){
+                return p;
+            }
+            p = p->next;
             q = q->next;
         }
         return nullptr;
