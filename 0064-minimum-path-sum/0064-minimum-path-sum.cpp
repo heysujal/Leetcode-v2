7f0994ee47d10ec12352c8eb227aab4e7067e2ff
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-07 11:53:24 +0530

    Time: 3 ms (97.14%), Space: 12.9 MB (5.41%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    // MODIFYING THE INPUT
-    int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        // filling up base cases
-        for (int i = 1; i < m; i++) {
-            grid[i][0] += grid[i-1][0];
+    int m;
+    int n;
+    int helperTab(vector<vector<int>> &grid){
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        // filling up base case i.e last row and last column
+        vector<int> next(n, 0);
+        next[n-1] = grid[m-1][n-1];
+        for(int col = n-2; col >= 0; col--){
+            next[col] = grid[m-1][col] + next[col+1];
         }
-        
-        for (int j = 1; j < n; j++) {
-            grid[0][j] += grid[0][j-1];
-        }
-        
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+        for(int i = m-2; i >= 0; i--){
+            vector<int> curr(n, 1e9);
+            curr[n-1] = grid[i][n-1] + next[n-1];
+            for(int j = n-2; j >= 0; j--){
+                curr[j] = grid[i][j] + min(curr[j+1], next[j]);
             }
+            next = curr;
         }
-        
-        return grid[m-1][n-1];
+        return next[0];
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        return helperTab(grid);
     }
 };
