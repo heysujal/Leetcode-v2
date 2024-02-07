4d8783ca182e5d0a6627898cfb7a451bd0270459
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-07 11:40:02 +0530

    Time: 7 ms (69.73%), Space: 12 MB (33.32%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int m;
-    int n;
-    int helperTab(vector<vector<int>> &grid){
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        // filling up base case i.e last row and last column
-        dp[m-1][n-1] = grid[m-1][n-1];
-        for(int col = n-2; col >= 0; col--){
-            dp[m-1][col] = grid[m-1][col] + dp[m-1][col+1];
+    // MODIFYING THE INPUT
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        // filling up base cases
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i-1][0];
         }
-        for(int row = m-2; row >= 0; row--){
-            dp[row][n-1] = grid[row][n-1] + dp[row+1][n-1];
+        
+        for (int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j-1];
         }
-        for(int i = m-2; i >= 0; i--){
-            for(int j = n-2; j >= 0; j--){
-                dp[i][j] = grid[i][j] + min(dp[i][j+1], dp[i+1][j]);
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
             }
         }
-        return dp[0][0];
-    }
-    int minPathSum(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        return helperTab(grid);
+        
+        return grid[m-1][n-1];
     }
 };
