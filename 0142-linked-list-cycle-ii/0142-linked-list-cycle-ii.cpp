775f4fb1bc374b76411187867ccb56663777fc7d
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-07 14:11:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,20 +8,29 @@
  */
 class Solution {
 public:
-    // using u_map
+    // slow and fast method (hint: cycle question)
+    // Algo
+    // detect cycle using slow and fast where they meet
+    // now move any of them to head and keep moving by one step.
     ListNode *detectCycle(ListNode *head) {
-        // ask interviwer 
-        // does this have unique values?, if yes store values instead of node in set
-
-        unordered_set<ListNode*> st;
-        ListNode* ptr = head;
-        while(ptr){
-            if(st.count(ptr)){
-                return ptr;
+        if(!head or !head->next)
+            return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast and fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                break;
             }
-            st.insert(ptr);
-            ptr = ptr->next;
         }
-        return nullptr;
+        // cycle is detected
+        // now we move one step at a time
+        fast = head;
+        while(slow != fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow;
     }
 };
