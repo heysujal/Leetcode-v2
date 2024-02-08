b222e1aabfe42e2ba97aff340274a2dff5a6f53b
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-08 12:41:32 +0530

    Time: 29 ms (95.12%), Space: 10.4 MB (42.39%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -15,9 +15,23 @@ public:
         }
         return dp[n] = min_count;
     }
-
+    int helperTab(int n){
+        vector<int> dp(n+1, 0);
+        // if amount is zero you need 0 coins
+        dp[0] = 0;
+        for(int i = 1; i <= n; i++){
+            int min_count = 1e9;
+            for(int j = 1; j*j <= i; j++){
+                int mini = 1 + dp[i-j*j];
+                min_count = min(min_count, mini);
+            }
+            dp[i] = min_count;
+        }
+        return dp[n];
+    }
     int numSquares(int n) {
-        vector<int> dp(n+1, -1);
-        return helper(n, dp);
+        // vector<int> dp(n+1, -1);
+        // return helper(n, dp);
+        return helperTab(n);
     }
 };
