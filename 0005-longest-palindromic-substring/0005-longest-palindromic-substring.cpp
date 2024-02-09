d3662413e7f43f19897265baadee68b92a896d83
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-09 14:27:33 +0530

    Time: 894 ms (11.34%), Space: 472.4 MB (5.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
+    // gives longest substring possible expanding from i, j
+    string expandAroundCentre(int i, int j, string &s){
+        int n = s.size();
+        string ans = "";
+        while((i >= 0 and j < n) and s[i] == s[j]){
+            if(ans.size() < j-i+1){
+                ans = s.substr(i, j-i+1);
+            }
+            i--;
+            j++;
+        }
+        return ans;
+    }
     string longestPalindrome(string s) {
         int n = s.size();
         string ans = "";
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        for(int diff = 0; diff < n; diff++){
-            for(int i = 0, j = diff; j < n; i++, j++){
-                if(diff == 0){
-                    dp[i][j] = true;
-                }
-                else if(diff == 1){
-                    dp[i][j] = (s[i] == s[j]);
-                }
-                else{
-                    dp[i][j] = (s[i] == s[j]) and dp[i+1][j-1];
-                }
-                if(dp[i][j] and (j-i+1) > ans.size()){
-                    ans = s.substr(i, j-i+1);
-                }
+        for(int i = 0; i < n; i++){
+            string odds = expandAroundCentre(i, i, s);
+            string evens = expandAroundCentre(i, i+1, s);
+            // for every starting we have evens and odds palindromes
+            if(ans.size() < evens.size()){
+                ans = evens;
+            }
+            if(ans.size() < odds.size()){
+                ans = odds;
             }
         }
         return ans;
