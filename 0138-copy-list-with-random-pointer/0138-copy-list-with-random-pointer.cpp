f8cf4d985fbc09412f4d7dc790fac03a0ea5eeeb
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-09 23:12:20 +0530

    Time: 10 ms (28.19%), Space: 12.9 MB (26.67%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,20 +16,44 @@ public:
 
 class Solution {
 public:
+    // algo
+    // insert nodes in b/w
+    // copy random pointer
+    // copy next pointer
+
     Node* copyRandomList(Node* head) {
-        // step1. Copy values in a new list
-        Node* ptr = head;
-        unordered_map<Node*, Node*> mp;
-        while(ptr){
-            mp[ptr] = new Node(ptr->val);
-            ptr = ptr->next;
+        Node *og = head;
+        while(og){
+            Node* copy = new Node(og->val);
+            copy->next = og->next;
+            og->next = copy;
+            og = copy->next;
+        }   
+
+        // copy random pointer
+        og = head;
+        while(og){
+            if(og->random){
+                og->next->random = og->random->next;
+            }
+            else{
+                og->next->random = nullptr;
+            }
+            og = og->next->next;
         }
-        ptr = head;
-        while(ptr){
-            mp[ptr]->next = mp[ptr->next];
-            mp[ptr]->random = mp[ptr->random];
-            ptr = ptr->next;
+
+        // copy next pointer
+        og = head;
+        Node* temp = new Node(0);
+        Node* aux = temp;
+        while(og){
+            //imp. to use temp this way maybe coz of single node case
+            temp->next = og->next;
+            temp = temp->next;
+            //...imp.
+            og->next = og->next->next;
+            og = og->next;
         }
-        return mp[head];
+        return aux->next;
     }
 };
