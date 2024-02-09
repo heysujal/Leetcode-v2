2b50be39b09c46946455365c5dc21aab2fca563d
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-09 17:42:40 +0530

    Time: 12 ms (94.52%), Space: 21 MB (21.13%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,34 +1,31 @@
 class MinStack {
 public:
-    stack<int> mainStack;
-    stack<int> minStack;
+    stack<int> main, aux;
     MinStack() {
-        
+
     }
     
     void push(int val) {
-        mainStack.push(val);
-        // equality symbol is because
-        // if you push same element twice in mainStack 
-        // them you have to push it twice in the minStack too. 
-        // because u are checking for equality when popping element.
-        if(minStack.empty() or (val <= minStack.top()))
-            minStack.push(val);
+        main.push(val);
+        if(aux.empty() or val <= aux.top()){
+            aux.push(val);
+        }
     }
     
     void pop() {
-        int val = mainStack.top();
-        mainStack.pop(); 
-        if(!minStack.empty() and (val == minStack.top()))
-            minStack.pop();
+        int val = main.top();
+        main.pop();
+        if(val == aux.top()){
+            aux.pop();
+        }
     }
     
     int top() {
-        return mainStack.top();
+        return main.top();
     }
     
     int getMin() {
-        return minStack.top();
+        return aux.top();
     }
 };
 
