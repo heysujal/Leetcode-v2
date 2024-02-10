ed63d955307ba86b484df72227b592e6c6902969
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-10 12:12:42 +0530

    Time: 4 ms (76.2%), Space: 7.4 MB (78.14%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-
-    // Tabulation solution is all about iterating diagonally
-    // short ref : https://youtu.be/biHfMygdoCE?t=1089
-    int countSubstrings(string s) {
+    int expandAroundCentre(int i, int j, string &s){
         int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        // we know all strings of 1 length(i,i) in matrix are
-        // palindromes
         int count = 0;
-
-        for(int diff = 0; diff < n; diff++){
-            for(int i = 0, j = diff; j < n; i++,j++){
-                    if(diff == 0){ // string of 1 len
-                        dp[i][j] = true;
-                    }
-                    else if(diff == 1){  // string of 2 len(j-i+1 = 2)
-                        dp[i][j] = (s[i] == s[j]);
-                    }
-                    else {
-                        dp[i][j] = (s[i] == s[j] and dp[i+1][j-1]);
-                    }
-                    if(dp[i][j]){
-                        count += 1;
-                    } // true or false;
-            }
+        while((i >= 0 and j < n) and (s[i] == s[j])){
+            count++;
+            i--;
+            j++;
         }
         return count;
+    }
+    int countSubstrings(string s) {
+        int n = s.size();
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            int odd = expandAroundCentre(i, i, s);
+            int even = expandAroundCentre(i, i+1, s);
+            ans += (odd + even);
+        }
+        return ans;
     }
 };
