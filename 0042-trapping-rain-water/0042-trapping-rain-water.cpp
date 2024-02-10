a061de589f49de7ec6b574548027b89739fae1fd
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-10 20:21:49 +0530

    Time: 18 ms (16.85%), Space: 22.1 MB (31.45%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    // for every bar find the left max and right max starting from itself
-    // after that min(left, right) - height[bar]
-    // will be added to final answer
+    // O(1) space approach
     int trap(vector<int>& height) {
         int n = height.size();
+        // Assuming left is the limiting factor!
+        // what if there is a even more smaller in between left and right
+        // Ans. it will be stopped by longer right bar
+        int left = 0;
+        int right = height.size()-1;
+        int left_max = 0;
+        int right_max = 0;
         int water = 0;
-        vector<int> left_max(n, 0);
-        vector<int> right_max(n, 0);
-        left_max[0] = height[0];
-        right_max[n-1] = height[n-1];
-        for(int i = 1; i < n; i++){
-            left_max[i] = max(left_max[i-1], height[i]);
-        }
-        for(int j = n-2; j >= 0; j--){
-            right_max[j] = max(right_max[j+1], height[j]);
-        }
-        for(int i = 0; i < n; i++){
-            water += min(right_max[i], left_max[i]) - height[i];
+        while(left <= right){
+            left_max = max(left_max, height[left]);
+            right_max = max(right_max, height[right]);
+            if(left_max < right_max){
+                water += left_max - height[left];
+                left++;
+            }
+            else{
+                water += right_max - height[right];
+                right--;
+            }
         }
         return water;
     }
