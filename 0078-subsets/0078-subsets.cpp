b23781879244a0bf7417483d8801029e66209acb
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-11 13:02:01 +0530

    Time: 0 ms (100%), Space: 8.2 MB (61.71%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    // Complexity = 2^n
-    void solve(int i , vector<int> &temp, vector<int> &nums, vector<vector<int>> &ans){
-        // base case
-        int n = nums.size();
-        if(i==n){
+    void helper(int i, vector<int> &temp, vector<vector<int>> &ans, vector<int> &nums){
+        if(i == nums.size()){
             ans.push_back(temp);
             return;
         }
+        // pick 
         temp.push_back(nums[i]);
-        solve(i+1, temp, nums, ans);
+        helper(i+1, temp, ans, nums);
         temp.pop_back();
         // skip
-        solve(i+1, temp, nums, ans);
-        // pick
+        helper(i+1, temp, ans, nums);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
-        solve(0, temp,nums,ans);
+        helper(0, temp, ans, nums);
         return ans;
     }
 };
