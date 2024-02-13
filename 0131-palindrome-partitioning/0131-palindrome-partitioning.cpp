d6e41b97c5d1b8719b54d281da58e36b01f9dee9
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-13 23:11:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -10,20 +10,12 @@ public:
             ans.push_back(temp);
             return;
         }
-        // here it is important to use this way
-        // because we are trying to partition and NOT creating
-        // some subsequence, so we need to have all characters
-        // that too from starting
-        // cut = 0 means giving whole string away
-        // cut = 1 means ["char0", "......restChars"]
-        // cut = 2 means ["char0char1", ".....restChars"]
-        for(int cut = i; cut < n; cut++){
-            // you make cuts here on the original string
-            // and apply cuts on rest of the partition by recursion
-            string str = s.substr(i, cut-i+1);
-            if(isPalindrome(str, 0, cut-i)){
+        // making cut of different lengths
+        for(int len = 1; len <= n; len++){
+            string str = s.substr(i, len);
+            if(isPalindrome(str, 0, len-1)){
                 temp.push_back(str);
-                helper(cut+1, n, temp, s, ans); // passing rest of the string
+                helper(i+len, n, temp, s, ans); // passing rest of the string
                 temp.pop_back();
             }
         }
