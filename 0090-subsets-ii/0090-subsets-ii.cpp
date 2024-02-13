2c94b5027dcd9b303a4b118462d88d02211413d0
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-13 19:25:10 +0530

    Time: 2 ms (68.29%), Space: 14.7 MB (22.31%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    void helper(int i, int n, vector<int> temp, vector<int> &nums, set<vector<int>> &ans){
+    void helper(int i, int n, vector<int> temp, vector<int> &nums, vector<vector<int>> &ans){
         if(i == n){
-            ans.insert(temp);
+            ans.push_back(temp);
             return;
         }
         temp.push_back(nums[i]);
         helper(i+1, n, temp, nums, ans);
         temp.pop_back();
+        // it is important that you never skip the first occurence
+        // or else you will lose on some subsets
+        while(i+1 < n and nums[i] == nums[i+1]){
+            i++;
+        }
         helper(i+1, n, temp, nums, ans);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> st;
         int n = nums.size();
-        helper(0, n, {}, nums, st);
-        vector<vector<int>> ans(st.begin(), st.end());
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        helper(0, n, {}, nums, ans);
         return ans; 
     }
 };
