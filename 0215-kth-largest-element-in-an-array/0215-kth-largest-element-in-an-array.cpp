6966bf66d94e792382406adcef5087b74480e428
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-23 21:53:48 +0530

    Time: 90 ms (71.89%), Space: 57.5 MB (83.84%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,7 +1,49 @@
 class Solution {
 public:
+
+    int partition(vector<int> &nums, int L, int R){
+        int P = nums[L];
+        int i = L+1;
+        int j = R;
+        // Good Order nums[i](all greater) > P > nums[j](all smaller)
+        while(i <= j){
+            if(nums[i] < P and nums[j] > P){
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
+            }
+            if(nums[i] >= P){
+                i++;
+            }
+            if(P >= nums[j]){
+                j--;
+            }
+        }
+        swap(nums[L], nums[j]);
+        return j;
+    }
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end(), greater<int>());
-        return nums[k-1];
+        int n = nums.size();
+        int L = 0;
+        int R = n-1;
+        int pivot_idx = 0; // assuming first element as pivot
+        
+        // k-th largest means nums[k-1] 
+        // in decending order after partitioning
+        while(true){ // i have no clue when this suffering will end
+
+            pivot_idx = partition(nums, L , R);
+            if(pivot_idx == k-1){
+                return nums[pivot_idx];
+            }
+            else if(pivot_idx > k-1){
+                // wanted 2nd but got 4th largest, so shrink
+                R = pivot_idx - 1;
+            }
+            else{
+                L = pivot_idx + 1;
+            }
+        }
+        return nums[pivot_idx];
     }
 };
