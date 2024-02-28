5726f75f976ba53a5bd3dfa898023f4379860c8e
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-28 14:36:04 +0530

    Time: 11 ms (42.78%), Space: 20.3 MB (51.68%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -1,36 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+//Approach-3 (Using BFS with inner while loop)
+//T.C : O(n)
+//S.C : O(n) for queue
 class Solution {
 public:
-    int height(TreeNode* root){
-        return !root ? 0 : 1 + max(height(root->left),height(root->right));
-    }
-    int ans;
-    bool flag = true;
-    void helper(TreeNode* root, int level){
-        if(!root or !flag){
-            return; 
-        }
-        if(level == 0 and flag){
-            ans = root->val;
-            flag = false;
-            return;
-        }
-        helper(root->left, level - 1);
-        helper(root->right, level - 1);
-    }               
     int findBottomLeftValue(TreeNode* root) {
-        int level = height(root)-1;
-        helper(root, level);
-        return ans;
+        queue<TreeNode*> que;
+        
+        int bottomLeft;
+        que.push(root);
+        
+        while(!que.empty()) {
+            int n = que.size();
+
+            while(n--) {
+                TreeNode* node = que.front();
+                que.pop();
+                
+                
+                bottomLeft = node->val;
+                
+                if(node->right) {
+                    que.push(node->right);
+                }
+                if(node->left) {
+                    que.push(node->left);
+                }
+            }
+            
+        }
+        
+        return bottomLeft;
     }
 };
