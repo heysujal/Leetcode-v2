b0d3a567afd9faf0fc6c68e81072eff3fc418dda
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-28 10:14:38 +0530

    Time: 12 ms (32.78%), Space: 19.9 MB (96.27%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -12,15 +12,17 @@
 class Solution {
 public:
     int height(TreeNode* root){
-        return !root ? 0 : 1 + height(root->left) + height(root->right);
+        return !root ? 0 : 1 + max(height(root->left),height(root->right));
     }
     int ans;
+    bool flag = true;
     void helper(TreeNode* root, int level){
-        if(!root){
+        if(!root or !flag){
             return; 
         }
-        if(level == 0){
+        if(level == 0 and flag){
             ans = root->val;
+            flag = false;
             return;
         }
         helper(root->left, level - 1);
