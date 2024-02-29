017bfcffb53e093273d0fb825756b97320332be5
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-02-29 12:46:09 +0530

    Time: 0 ms (100%), Space: 15.2 MB (55.33%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,41 +1,21 @@
 class Solution {
 public:
 
-    bool isValid(string &s){
-        stack<char> st;
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '('){
-                st.push('(');
-            }
-            else{
-                if(st.empty()){
-                    return false;
-                }
-                if(st.top() == '('){
-                    st.pop();
-                }
-            }
-        }
-        return st.empty();
-    }
-    void helper(int i, int n, string temp, vector<string> &ans){
-        if(i == 2*n){
-            if(isValid(temp)){
-                ans.push_back(temp);
-            }
+    void helper(int open, int close, int n, string temp, vector<string> &ans){
+        if(temp.size() == 2*n){ // or close == n
+            ans.push_back(temp);
             return;
         }
-        // push '('
-        temp += "(";
-        helper(i+1, n, temp, ans);
-        temp.pop_back();
-        temp += ")";
-        helper(i+1, n, temp, ans);
-        temp.pop_back();
+        if(open < n){
+            helper(open+1, close, n, temp + '(', ans);
+        }
+        if(close < open){
+            helper(open, close+1, n, temp + ')', ans);
+        }
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        helper(0, n, "", ans);
+        helper(0,0, n, "", ans);
         return ans;
     }
 };
