d3b6feae5747ab36eb2bca4647717c94f7cc92c7
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-03-04 10:10:27 +0530

    Time: 4 ms (74.87%), Space: 13.1 MB (55.08%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        int n = tokens.size();
+        if(n == 0){
+            return 0;
+        }
+        int left = 0;   // Lose Power, Gain Score from here
+        int right = n-1; // Lose Score, Gain Power from here
+        sort(tokens.begin(), tokens.end());
+        if(power < tokens[0]){
+            return 0;
+        }
+        int score = 0;
+        while(left < right){
+            while(left < right and power >= tokens[left]){
+                power -= tokens[left];
+                score++;
+                left++;
+            }
+            if(power < tokens[left]){
+                while(left < right and power < tokens[left]){
+                    power += tokens[right];
+                    score--;
+                    right--;
+                }
+            }
+        }
+        if(left == right and power >= tokens[left]){
+            return score + 1;
+        }
+        return score;
+    }
+};
