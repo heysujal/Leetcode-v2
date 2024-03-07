cbb92c09638274e760de7acf777dacf2604fe5ec
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-03-07 13:17:01 +0530

    Time: 23 ms (5.71%), Space: 19.6 MB (5.94%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,29 +1,84 @@
 class Solution {
 public:
+    // Using Trie
+    class TrieNode{
+        public :
+            char data;
+            TrieNode *children[26];
+            int childCount;
+            bool isTerminal;
+        TrieNode(char ch){
+            data = ch;
+            for(int i = 0; i < 26; i++){
+                children[i] = nullptr;
+            }
+            childCount = 0;
+            isTerminal = false;
+        }
 
-    string common(string p, string q){
-        int n = min(p.size(), q.size());
-        string res = "";
-        int i = 0;
-        while(i < n){
-            if(p[i] == q[i]){
-                res.push_back(p[i]);
+    };
+
+    class Trie{
+        public : 
+            TrieNode* root;
+            Trie(char ch){
+                root = new TrieNode(ch);
+            }
+
+            void insertUtil(TrieNode *root, string word){
+                // base case
+                if(word.size() == 0){
+                    root->isTerminal = true;
+                    return;
+                }
+
+                int idx = word[0] - 'a';
+                // character doesn't exist already
+                if(root->children[idx] == nullptr){
+                    root->children[idx] = new TrieNode(word[0]);
+                    root->childCount++;
+                }
+                // passing on this node to rest of the characters
+                insertUtil(root->children[idx], word.substr(1));
+            }
+
+            void insert(string word){
+                insertUtil(root, word);
+                return;
+            }
+
+            void lcp(string s, string &ans){
+                for(int i = 0; i < s.size(); i++){
+                    if(root->childCount == 1){
+                        char ch = s[i];
+                        int idx = ch - 'a';
+                        ans += ch;
+                        root = root->children[idx];
+                    }
+                    else{
+                        break;
+                    }
+                    if(root->isTerminal == true){
+                        break;
+                    }
+                }
+            }
+    };
+    string longestCommonPrefix(vector<string>& strs) {
+        Trie *t = new Trie('\0'); // this is root;
+        for(auto &word : strs){
+            if(word == ""){
+                return "";
             }
             else{
-                break;
+                t->insert(word);
             }
-            i++;
         }
-        return res;
-    }
 
-    string longestCommonPrefix(vector<string>& strs) {
-        // assuming the first string to be ans
-        string ans = strs[0];
-        // now we try to find common with other strings
-        for(int i = 1; i < strs.size(); i++){
-            ans = common(ans, strs[i]);
-        }
+        // assume first one to be the answer
+        string first = strs[0];
+        string ans = "";
+        t->lcp(first, ans);
         return ans;
     }
 };
