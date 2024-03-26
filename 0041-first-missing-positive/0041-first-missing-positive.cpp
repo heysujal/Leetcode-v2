6d3b47364a243b4c48d9f5de7f3f684aa4d371b6
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-03-26 14:49:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
-        // Missing POSITIVE([1,n])
+    int firstMissingPositive(vector<int>& arr) {
         int i = 0;
-        int n = nums.size();
-        while(i<n){
-            // Try avoiding any +1 and -1 to avoid overflow
-            // nums[i]-1 >= 0 => nums[i] >= 1 => nums[i] > 0
-            // nums[i]-1 < n => nums[i] < n+1 => nums[i] <= n
-            if(nums[i] > 0 and nums[i] <= n and nums[i] != nums[nums[i]-1])
-                swap(nums[i], nums[nums[i]-1]);
-            else
+        int n = arr.size();
+        while(i < n){
+            if(arr[i]-1 < n and arr[i] - 1 >= 0 and arr[i] != arr[arr[i] - 1]){
+                swap(arr[i], arr[arr[i] - 1]);
+            }
+            else{
                 i++;
+            }
         }
-        // Array should be like this 1,2,3,-1,5,0
-        for(int i = 1 ; i <= n ; i++)
-            if(i != nums[i-1])
-                return i;
-        // Test case : [1]
-        // 2 is missing
-        return n+1;
+
+        for(int i = 0; i < n; i++){
+            if(i+1 != arr[i]){
+                return i+1;
+            }
+        }
+        return -1;
     }
 };
