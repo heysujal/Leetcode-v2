74b8f5babd90f13a21beb98ca2b7e17bfb52662e
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-03-27 22:17:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -11,31 +11,45 @@
  */
 class Solution {
 public:
-    int levelOrderBFS(TreeNode* root){
-        if(!root)
-            return 0;
-        int maxWidth = 0;
-        queue<pair<TreeNode*,int>> q; // node, position
-        q.push({root,1});
-        while(!q.empty()){  
-            int size = q.size();
-            vector<long long> v; // to store positions of nodes of a level
-            for(int i = 0; i < size ; i++){
-                auto front = q.front();
-                q.pop();
-                TreeNode* node = front.first;
-                int idx = front.second;
-                v.push_back(idx); // current position
-                if(node->left)
-                    q.push({node->left,(long long)2*idx});
-                if(node->right)
-                    q.push({node->right,(long long)2*idx+1});
-            }
-            maxWidth = max(maxWidth,(int)(v.back()-v.front()+1));
-        }
-        return maxWidth;
-    }
     int widthOfBinaryTree(TreeNode* root) {
-        return levelOrderBFS(root);
+        if(!root){
+            return 0;
+        }
+        // 0 based indexing
+        int width = 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+
+        while(!q.empty()){
+            // we want leftMost idx and rightMost idx 
+            // for every level
+            int leftMost;
+            int rightMost;
+            // this is the leftMost node of every level
+            // we use it as a reference/origin to find distance
+            int origin = q.front().second;
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                auto node = q.front().first;
+                auto idx = q.front().second - origin;
+                q.pop();
+                if(i == 0){
+                    leftMost = idx;
+                }
+                if(i == size - 1){
+                    rightMost = idx;
+                }
+                if(node->left){
+                    q.push({node->left, 2*idx + 1});
+                }
+                if(node->right){
+                    q.push({node->right, 2*idx + 2});
+                }
+            }
+            // level is processed 
+            // now, do right - left + 1
+            width = max(width, rightMost - leftMost + 1);
+        }
+        return width;
     }
 };
