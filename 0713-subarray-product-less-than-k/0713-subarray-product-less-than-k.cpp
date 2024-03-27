8d0439f11899a14377b84a8a8b810c225e953bab
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-03-27 15:53:38 +0530

    Time: 106 ms (16.04%), Space: 61.1 MB (100%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        // brute force
-        // make all subarrays
-        int n = nums.size();
-        set<vector<int>> st;
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            int prod = 1;
-            for(int j = i; j < n; j++){
-                // the below loop iterates over each subarray
-                prod *= nums[i];
-                if(prod < k){
-                    vector<int> v;
-                    v.insert(v.end(), nums.begin() + i, nums.begin() + i + j);
-                    st.insert(v);
-                }
-                else{
-                    break;
-                }
+        // Number of subarrays will be given by head-tail+1
+        // i.e. length of subarray because 
+        // this time all the subarrays will include a new
+        // character/element present at head
+        // All the subarrays for previous elements will 
+        // be covered already by this time.  
+        
+        int head = 0;
+        int tail = 0;
+        int ans = 0;
+        int prod = 1;
+        // if k is 0 or 1 then return 0 arr[i] >= 1
+        if(k == 1 or k == 0)
+            return 0;
+        for(head=0;head<nums.size();head++){       
+            prod*=nums[head];
+            while(prod >= k){
+                prod/=nums[tail];
+                tail++;
             }
+            ans = ans + head-tail+1;
         }
-        return st.size();
+        return ans;
     }
 };
