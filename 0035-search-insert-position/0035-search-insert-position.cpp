736e4167f0f14c508887e0d11800896db9c8a4bc
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2024-04-26 10:41:50 +0530

    Time: 0 ms (100%), Space: 12.2 MB (7.73%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-    int searchInsert(int A[], int n, int target) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        
-        int p=mod_binsearch(A,target,0,n);
-        return p;
-      
-    }
-    
-    
-    
-    int mod_binsearch(int A[],int target,int l,int n)
-    {
-            int low=0,high=n;
-           int mid;
-        
-     
-         while (low <= high)
-         {
-        mid=(low+high)/2;
-      
-              if (A[mid] == target)
-                    return mid   ;         
-              else if (A[mid] < target)
-                 low = mid+1;
-              else
-                 high = mid-1;
+    // Lower Bound
+    // find smallest index for which x <= arr[index]
+    // So, we try to store potential answer
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        if(target < nums[0]){
+            return 0;
         }
-        
-       if(target>mid) mid++;
-        return mid;
-        else
-        mid--;
-        return mid;
-        
-   
-        
+        else if(target > nums[n-1]){
+            return n;
+        }
+
+        int ans = 0;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target <= nums[mid]){
+                // store potential answer
+                // look for smaller index
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 };
